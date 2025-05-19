fb6165731c66e495d990eb53c0b7a8fc7778098b
Harshad M T <harshadmuhammad168@gmail.com>
2025-05-19 17:27:34 +0530

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 function strings (str){
-  let b = str.split(',').join('')
- let a = Number(b)
-  
-   return b
+  let b = str.split(',')
+ let a = b.reduce((a,b)=>a*b)
+return a ;
 }
 
 let str = "3,4"
